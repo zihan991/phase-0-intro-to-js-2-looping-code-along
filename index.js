8de8e09044f0af328ea61b1e08dea70a2079a8e4
ittehadzihan <zihan991@gmail.com>
2022-07-21 21:23:58 -0400

    DONE

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const cards = ["Lisa", "Kaitlyn", "Jan"];
+
+const newArray = []
+function writeCards(cards){
+    for( let i = 0; i < cards.length; i++){
+        newArray.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+    }
+  return newArray
+}
+
+function  countDown(integer){
+    while(integer >= 0){
+        console.log(integer);
+        integer--;  
+    }
+}
+
+countDown(4)
